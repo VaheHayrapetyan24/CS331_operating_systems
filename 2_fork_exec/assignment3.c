bce6d5b4cffdeebaffc6d24207f0321cd99adddb
VaheHayrapetyan24 <vahe.hayrapetyanusa@gmail.com>
2025-09-09 22:38:54 +0400

    feat: add hw2 assigmnent 3

---
+++ 2_fork_exec/assignment3.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+    int res = fork();
+    if (res == 0) {
+        execl("/usr/bin/echo", "echo", "hello from the child process\n", NULL);
+        perror("execl");
+        return 0;
+    }
+
+    printf("Parent process done \n");
+}
