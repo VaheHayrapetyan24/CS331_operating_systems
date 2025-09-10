d3a2d48fbf6d8b344e12b9c38e2306d1b3236ce0
VaheHayrapetyan24 <vahe.hayrapetyanusa@gmail.com>
2025-09-10 21:06:48 +0400

    feat: add assignment 4

---
+++ 2_fork_exec/assignment4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main() {
+    int res = fork();
+    if (res == 0) {
+        execl("/usr/bin/grep", "grep",  "main", "./2_fork_exec/test.txt", NULL);
+        perror("execl");
+        return 0;
+    }
+
+    printf("Parent process done \n");
+}
